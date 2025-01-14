d1668bf7eddcf83d65fe916abaa85c25f82789b2
aniflixmusic <aniflixmusic@gmail.com>
2025-01-14 21:04:39 +0530

    Update couples.py

--- SONALI/plugins/tools/couples.py
+++ SONALI/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from SONALI.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="ꪜ 𝛊 ɭ ɭ ᧘ 𝛊 𝛈",
-            url=f"https://t.me/iamakki001",
+            text="𝙎𝙪𝙥𝙥𝙤𝙧𝙩",
+            url=f"https://t.me/SST_ANIME_GROUP",
         ),
     ],
 ]
